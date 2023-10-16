f5f29e3e6e8590dbe39e976363fab6d84a4791d6
BrenC222 <brencdev.io@gmail.com>
2023-10-16 13:08:27 -0500

    Modularized the program

---
+++ module_1/write_to_file.py
@@ -0,0 +1,13 @@
+import os
+
+def write_text_file(text_data, output_file):
+    cur_dir = os.getcwd()
+    lf_dir = "Data/raw"
+    file_path = os.path.join(cur_dir, lf_dir, output_file)
+    try:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(text_data)
+        print(f"Text download and saved to file {output_file}")
+    
+    except Exception as e:
+        print(f"An error occured: {str(e)}")
